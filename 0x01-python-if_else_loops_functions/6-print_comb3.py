779083901a9c2303a05e81002782351715c839b1
Afonne-CID <delightinbusiness@gmail.com>
2021-09-08 07:39:13 -0700

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for num in range(100):
-    if num == 99:
-        print("{}".format(num))
-    else:
-        print("{:02}".format(num), end=", ")
+for num in range(0, 10):
+    for num1 in range(num + 1, 10):
+        if num == 8 and num1 == 9:
+            print("{}{}".format(num, num1), end="\n")
+        else:
+            print("{}{}".format(num, num1), end=", ")
