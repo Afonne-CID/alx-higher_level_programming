b5733d92f3e007a43c841368c27e09802ba57f93
Afonne-CID <delightinbusiness@gmail.com>
2021-09-14 07:49:25 -0700

    Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,6 +5,7 @@ void print_python_list_info(PyObject *);
 /**
  * print_python_list_info - Prints basic info about Python lists.
  * @p: A PyObject list.
+ *
  */
 void print_python_list_info(PyObject *p)
 {
