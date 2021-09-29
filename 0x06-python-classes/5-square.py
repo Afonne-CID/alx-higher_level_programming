8ffd7acc2ef4c1c1b49443b12f405c1c4a972cd4
Afonne-CID <delightinbusiness@gmail.com>
2021-09-29 09:18:18 -0700

    more methods

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Writing a class that defines a square"""
+
+
+class Square:
+    """A class to represent a square"""
+    def __init__(self, size=0):
+        """Defining a private instance attribute, size
+
+        Args:
+            size(int): the size of our square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """retrieves our size attribute"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """sets the value of our private size attribute,
+        having confirmed size is and int and greater than zero,
+        otherwise, an error is raised
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """A public instance area that returns the current
+        square area
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("{}".format('#'), end="")
+            print("")
