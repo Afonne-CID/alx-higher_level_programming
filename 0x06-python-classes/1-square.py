72f1322b59debf7306af1abf7d6208cae62ffe57
Afonne-CID <delightinbusiness@gmail.com>
2021-09-29 08:03:03 -0700

    private attributes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,7 +6,7 @@ class Square:
     '''A class to represent a square.'''
     def __init__(self, size):
         '''We are keeping our instance attribute "size" private
-        because in this case, we are creating a square which 
+        because in this case, we are creating a square which
         will require a size. Since the size of a square is crucial
         and things like area computation depend of it.
         '''
