e55a5f7ac106eb35bd6e252b0e583b2645b26527
Afonne-CID <delightinbusiness@gmail.com>
2021-09-29 07:19:26 -0700

    fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+'''Define an empty class, called Square
+It's recommended classes names start with a Capital letter
+'''
 
 
 class Square:
-    '''An empty class that defines a square'''
+    '''An empty class that defines a square.'''
     pass
