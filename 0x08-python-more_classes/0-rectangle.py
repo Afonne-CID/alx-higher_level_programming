471fdea4ef6c2021e64821a074fbfa553bf2918b
Afonne-CID <delightinbusiness@gmail.com>
2021-10-05 02:21:47 -0700

    initial commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""This is the ``0-rectangle`` module
+
+The ``0-rectange`` module supplies an empty class, Rectange.
+"""
+
+
+class Rectangle:
+    """defines an empty class"""
+    pass
