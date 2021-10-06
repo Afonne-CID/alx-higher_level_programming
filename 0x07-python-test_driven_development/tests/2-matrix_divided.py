b4bbfe79d97aef0f83ed3de3010ba19a65a71302
Afonne-CID <delightinbusiness@gmail.com>
2021-10-06 13:45:12 -0700

    fix

--- 0x07-python-test_driven_development/tests/2-matrix_divided.py
+++ 0x07-python-test_driven_development/tests/2-matrix_divided.py
@@ -23,7 +23,7 @@ def matrix_divided(matrix, div):
     if (not isinstance(matrix, list) or matrix == [] or
             not isinstance(matrix[0], list)):
         raise TypeError("matrix must be a matrix (list of lists)"
-                        "of integers/floats")
+                        " of integers/floats")
     for row in range(0, len(matrix)):
         if len(matrix[row]) != len(matrix[0]):
             raise TypeError(
@@ -31,5 +31,5 @@ def matrix_divided(matrix, div):
         for value in matrix[row]:
             if not isinstance(value, int) and not isinstance(value, float):
                 raise TypeError("matrix must be a matrix (list of lists)"
-                                "of integers/floats")
+                                " of integers/floats")
     return ([[round(x/div, 2) for x in row] for row in matrix])
