0cd643c54e077299fb44d9b5895131705bf3a46f
Afonne-CID <delightinbusiness@gmail.com>
2021-10-06 13:30:48 -0700

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,11 +19,8 @@ def add_integer(a, b=98):
     >>> add_integer(10, 7)
     17
     """
-    if not isinstance(a, int) and not isinstance(a, float) or a is None:
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    else:
-        a = int(a)
-        b = int(b)
-    return (a + b)
+    return (int(a) + int(b))
--- 0x07-python-test_driven_development/tests/0-add_integer.py
+++ 0x07-python-test_driven_development/tests/0-add_integer.py
@@ -5,6 +5,7 @@ This is the 0-add_integer module.
 The 0-add_integer module supplies one function, add_integer(). For example,
 """
 
+
 def add_integer(a, b=98):
     """ Returns the sum of a and b.
 
@@ -18,11 +19,8 @@ def add_integer(a, b=98):
     >>> add_integer(10, 7)
     17
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if (not isinstance(a, int) and isinstance(a, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b must be an integer")
-    else:
-        a = int(a)
-        b = int(b)
-    return (a + b)
+    return (int(a) + int(b))
