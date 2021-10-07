511cbecaa0d29ca4ae574dcd9a1925df11440543
Afonne-CID <delightinbusiness@gmail.com>
2021-10-06 20:19:42 -0700

    input 0 handled

--- 0x07-python-test_driven_development/tests/4-print_square.py
+++ 0x07-python-test_driven_development/tests/4-print_square.py
@@ -23,5 +23,5 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
-        [print("#", end="") for j in range(size)]
+        print("#" * size, end="")
         print("")
