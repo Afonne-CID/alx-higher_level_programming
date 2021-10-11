4c1703c5fcdda1f9d5e909e2979db50e505a9406
Afonne-CID <delightinbusiness@gmail.com>
2021-10-11 09:32:55 -0700

    sort

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class MyList that inherits from ``list``"""
+
+
+class MyList(list):
+    """Inherits and extends ``list``"""
+    def print_sorted(self):
+        """Prints a list in sorted (ascending) order"""
+        return (sorted(self))
