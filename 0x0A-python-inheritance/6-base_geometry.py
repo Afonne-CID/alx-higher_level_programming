2768cbbffaacd3eccb3b107c7112b6d55b260a7e
Afonne-CID <delightinbusiness@gmail.com>
2021-10-11 11:37:14 -0700

    raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represents a base geometry"""
+
+    def area(self):
+        """Raises an excepton"""
+        raise Exception("area() is not implemented")
