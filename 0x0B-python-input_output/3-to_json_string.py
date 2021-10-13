b7f2fba4f46ebeccb3f1d2217144aa721e977aed
Afonne-CID <delightinbusiness@gmail.com>
2021-10-12 20:08:35 -0700

    fix

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """Defines a function that returns the JSON of a string"""
 import json
 
+
 def to_json_string(my_obj):
     """"Retuns the JSON representation of a string"""
     return json.dumps(my_obj)
