baa3706ceec32bd92e68923c5492b0a11ad1b9fd
Afonne-CID <delightinbusiness@gmail.com>
2021-10-16 03:15:42 -0700

    Rectangle class with methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,95 @@
 #!/usr/bin/python3
 """Defines a Rectangle"""
+from models.base import Base
 
 
-class Rectangle:
-    """Represents a Rectangle"""
+class Rectangle(Base):
+    """Represents a Rectangle
 
+    Inherits from the Base class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Class constructor
+
+        Args:
+            width (int): Rectangle's width
+            height (int): Rectangle's height
+            x (int): instance attribute x
+            y (int): instance attribute y
+            id (int): Rectangle's identity
+
+        Raises:
+            TypeError: If height or width is not an int
+            TypeError: If either x or y is not an int
+            ValueError: If height or width is <= 0
+            ValueError: If either x or y is under 0
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Retrieves Rectangle width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of Rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves Rectangle height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of Rectangle"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Retrieves the value of x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets the value of x"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Retrieves the value of y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets the value of y"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Returns the area value of the Rectangle instance"""
+        return self.width * self.height
+
+    def display(self):
+        for i in range(self.height):
+            print(self.width * "{}".format('#'))
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,14 +6,84 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     """Base unittest class"""
-    def test_arg(self):
+
+    def test_None(self):
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_three_instances(self):
         b1 = Base()
         b2 = Base()
         b3 = Base()
         self.assertEqual(b1.id, b2.id-1)
         self.assertEqual(b1.id, b3.id-2)
 
-    def test_None(self):
-        b1 = Base(None)
-        b2 = Base(None)
-        self.assertEqual(b1.id, b2.id - 1)
+    def test_two_instances(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id-1)
+
+    def test_id_public(self):
+        b = Base(12)
+        b.id = 15
+        self.assertEqual(15, b.id)
+
+    def test_str_id(self):
+        self.assertEqual("hello", Base("hello").id)
+
+    def test_private_instance(self):
+        with self.assertRaises(AttributeError):
+            print(Base(12).__nb_instances)
+
+    def test_float_id(self):
+        self.assertEqual(5.5, Base(5.5).id)
+
+    def test_complex_id(self):
+        self.assertEqual(complex(5), Base(complex(5)).id)
+
+    def test_bool_id(self):
+        self.assertEqual(True, Base(True).id)
+
+    def test_list_id(self):
+        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
+
+    def test_tuple_id(self):
+        self.assertEqual((1, 2, 3), Base((1, 2, 3)).id)
+
+    def test_set_id(self):
+        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
+
+    def test_frozenset_id(self):
+        self.assertEqual(frozenset({1, 2, 3}), Base(frozenset({1, 2, 3})).id)
+
+    def test_range_id(self):
+        self.assertEqual(range(5), Base(range(5)).id)
+
+    def test_bytes_id(self):
+        self.assertEqual(b'Paul', Base(b'Paul').id)
+
+    def test_bytearray_id(self):
+        self.assertEqual(bytearray(b'Python'), Base(bytearray(b'Python')).id)
+
+    def test_memoryview_id(self):
+        self.assertEqual(memoryview(b'abcefg'), Base(memoryview(b'abcefg')).id)
+
+    def test_inf_id(self):
+        self.assertEqual(float('inf'), Base(float('inf')).id)
+
+    def test_NaN_id(self):
+        self.assertNotEqual(float('nan'), Base(float('nan')).id)
+
+    def test_unique_id(self):
+        self.assertEqual(12, Base(12).id)
+
+    def test_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
+
+    def test_double_args(self):
+        with self.assertRaises(TypeError):
+            Base(1, 2)
