cdb4e96a0a8f693893451163cb5d25e7feb13f95
Afonne-CID <delightinbusiness@gmail.com>
2021-12-15 12:55:14 -0800

    edit

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurrences = function (list, searchElement) {
   let cnt = 0;
 
   for (const val of list) {
-    if (val === searchElement) cnt += 1;
+    if ((val && searchElement) && (val === searchElement)) cnt += 1;
   }
   return (cnt);
 };
