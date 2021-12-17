c7fac0b6a9d6ff55dddf1b63f8a469f1615dd2be
Afonne-CID <delightinbusiness@gmail.com>
2021-12-17 06:46:20 -0800

    Case sensitive search (string)

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all `states` with a `name` starting with\
+            'N'(Case sensitive) from `hbtn_0e_0_usa`
+   Usage: ./0-select_states.py\
+            <mysql username>\
+            <mysql password>\
+            <mysql db name>
+
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+                FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+
+    [print(state) for state in cur.fetchall()]
+
+    cur.close()
+    db.close()
