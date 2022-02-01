8363f5a196c256709523dcc365b13935bb5e4670
Afonne-CID <delightinbusiness@gmail.com>
2022-02-01 05:20:22 -0800

    fetch url with  package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches `https://intranet.hbtn.io/status` using the `request` package
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    value = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(value.text)))
+    print("\t- content: {}".format(value.text))
