225b8f8513d16cda1ff84281cf57decb5531b35f
Afonne-CID <delightinbusiness@gmail.com>
2022-02-01 04:23:58 -0800

    edit script

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends JSON file via `POST` request to a URL.
-curl -sH "Content-Type: application/json" -d "$(cat $2")" "$1"
+curl -sH "Content-Type: application/json" -d "$(cat "$2")" "$1"
