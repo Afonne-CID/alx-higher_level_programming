ffb2739b3ae955ce9f5239c02833840fe4c689d3
Afonne-CID <delightinbusiness@gmail.com>
2022-02-01 03:57:01 -0800

    editted

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
+"""Peak finding algorithm with the lowest complexity"""
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """Returns a peak in a list of unsorted integers"""
+    if (list_of_integers == []):
         return None
 
     size = len(list_of_integers)
-    if size == 1:
+    if (size == 1):
         return list_of_integers[0]
-    elif size == 2:
+    if (size == 2):
         return max(list_of_integers)
+    
+    pivot = int(size / 2)
+    peak = list_of_integers[pivot]
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+    if peak > list_of_integers[pivot - 1] and peak > list_of_integers[pivot + 1]:
         return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    elif peak < list_of_integers[pivot - 1]:
+        return find_peak(list_of_integers[:pivot])
     else:
-        return find_peak(list_of_integers[mid + 1:])
+        return find_peak(list_of_integers[pivot + 1:])
