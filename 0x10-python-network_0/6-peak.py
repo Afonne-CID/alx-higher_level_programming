d19cbfabb8daccbe58da6228a23ba40b679181bb
Afonne-CID <delightinbusiness@gmail.com>
2022-02-01 04:03:55 -0800

    fix whitespace issues

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Peak finding algorithm with the lowest complexity"""
+"""Peak finding algorithm with the lowest complexity."""
 
 
 def find_peak(list_of_integers):
-    """Returns a peak in a list of unsorted integers"""
-    if (list_of_integers == []):
+    """Returns a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
 
     size = len(list_of_integers)
@@ -12,11 +12,12 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     if (size == 2):
         return max(list_of_integers)
-    
+
     pivot = int(size / 2)
     peak = list_of_integers[pivot]
 
-    if peak > list_of_integers[pivot - 1] and peak > list_of_integers[pivot + 1]:
+    if (peak > list_of_integers[pivot - 1]
+            and peak > list_of_integers[pivot + 1]):
         return peak
     elif peak < list_of_integers[pivot - 1]:
         return find_peak(list_of_integers[:pivot])
