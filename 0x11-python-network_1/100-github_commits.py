ec050e0d04ac992533803d4113d627933c6a5ebd
Afonne-CID <delightinbusiness@gmail.com>
2022-02-01 10:37:16 -0800

    re-ordered order of arguments

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,7 +11,7 @@ import requests
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[1], sys.argv[2])
+            sys.argv[2], sys.argv[1])
 
     commits = requests.get(url).json()
     try:
