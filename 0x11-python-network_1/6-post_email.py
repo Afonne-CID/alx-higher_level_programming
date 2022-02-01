f4303442014f2e8be99f4809bb365c252e447419
Afonne-CID <delightinbusiness@gmail.com>
2022-02-01 05:31:55 -0800

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,9 +2,9 @@
 """ Send parameters in a `POST` request to a given URL.
 
     Usasge:
-        ./2-post-email.py <URL> <email>
+        ./6-post-email.py <URL> <email>
     Return:
-        Displays the body of the response (decoded in utf-8)
+        Displays the body of the response
 """
 import sys
 import requests
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     input_url = sys.argv[1]
     email = {"email": sys.argv[2]}
 
-    value = requests.post(url, data=email)
+    value = requests.post(input_url, data=email)
     print(value.text)
